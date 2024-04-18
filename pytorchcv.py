456786efd5d0b90601a3ceaf7d96a5c5824e9f36
2wnsqo <164177516+2wnsqo@users.noreply.github.com>
2024-04-18 18:11:21 +0000

    0419 3.11

--- pytorchcv.py
+++ pytorchcv.py
@@ -24,16 +24,16 @@ default_device = 'cuda' if torch.cuda.is_available() else 'cpu'
 # 이 함수를 실행하면, builtins 모듈을 통해 전역 변수로 설정된 data_train, data_test, train_loader, test_loader가 생성되어 어디서든 접근할 수 있게 됩니다. 이러한 설정은 함수 내에서 데이터를 처리하고, 이후에 다른 부분에서 해당 데이터를 사용할 때 유용하게 활용될 수 있음
 
 def load_fashion_mnist(batch_size=64): # load_fashion_mnist라는 이름의 함수를 정의하고, 이 함수는 기본적으로 batch_size 매개변수를 64로 설정합니다. 이 매개변수는 데이터를 얼마나 많은 단위로 나눌지 결정
-    data_full = torchvision.datasets.FashionMNIST('./data',download=True, train=True, transform=ToTensor()) # torchvision 라이브러리의 datasets 모듈을 사용하여 FasionMNIST 데이터셋을 불러옵니다. './data'는 데이터셋이 저장될 경로를 지정하며, download=True는 해당 경로에 데이터가 없을 경우 인터넷에서 자동으로 다운로드하도록 설정합니다. train=True는 학습용 데이터셋을 불러오는 것을 의미하고, transform=ToTensor()는 데이터셋의 이미지들을 파이토치 텐서로 변환하는 함수를 적용
-    # 전체 데이터셋을 학습용, 검증용, 테스트용으로 분할합니다.
-    train_size = int(0.6 * len(data_full))
-    val_size = int(0.2 * len(data_full))
-    test_size = len(data_full) - train_size - val_size
-    builtins.data_train, builtins.data_test, builtins.data_val = torch.utils.data.random_split(data_full, [train_size, val_size, test_size])
+    builtins.data_full = torchvision.datasets.FashionMNIST('./data',download=True, train=True, transform=ToTensor()) # torchvision 라이브러리의 datasets 모듈을 사용하여 FasionMNIST 데이터셋을 불러옵니다. './data'는 데이터셋이 저장될 경로를 지정하며, download=True는 해당 경로에 데이터가 없을 경우 인터넷에서 자동으로 다운로드하도록 설정합니다. train=True는 학습용 데이터셋을 불러오는 것을 의미하고, transform=ToTensor()는 데이터셋의 이미지들을 파이토치 텐서로 변환하는 함수를 적용
+    # 전체 데이터셋을 학습용, 검증용, 테스트용으로 분할합니다. (6:2:2)비율 사용
+    train_size = int(0.6 * len(data_full)) # train data의 비율을 0.6을준다
+    val_size = int(0.2 * len(data_full)) # val data의 비율을 0.2를 준다
+    test_size = len(data_full) - train_size - val_size # test data의 비율을 남은 비율을 준다. 
+    builtins.data_train, builtins.data_test, builtins.data_val = torch.utils.data.random_split(data_full, [train_size, val_size, test_size]) # 설정한 비율로 전체 데이터에서 랜덤으러 나눠준다. 
 
     builtins.train_loader = torch.utils.data.DataLoader(data_train, batch_size=batch_size,shuffle=True) # 학습 데이터셋을 데이터 로더에 로드합니다. 데이터 로더는 데이터셋을 지정된 배치 크기에 맞게 나누고, 이를 반복 가능한 객체로 만들어 학습 과정에서 쉽게 사용할 수 있게 도움
-    builtins.test_loader = torch.utils.data.DataLoader(data_test, batch_size=batch_size)
-    builtins.val_loader = torch.utils.data.DataLoader(data_val, batch_size=batch_size)
+    builtins.test_loader = torch.utils.data.DataLoader(data_test, batch_size=batch_size) # 테스트 데이터셋을 데이터 로더에 로드합니다.
+    builtins.val_loader = torch.utils.data.DataLoader(data_val, batch_size=batch_size) # 확인 데이터셋을 데이터 로더에 로드합니다.
     
     
 # 신경망을 한 에폭(epoch) 동안 학습하는 과정을 구현한 Python 함수
@@ -119,12 +119,12 @@ def plot_results(hist): # plot_results라는 함수를 정의하는데 hist라�
     plt.subplot(121) # 두 개의 그래프를 나란히 표시하기 위해 첫 번째 위치(1행 2열의 첫 번째)에 서브플롯을 생성
     plt.plot(hist['train_acc'], label='Training acc') # hist 딕셔너리에서 학습 정확도(train_acc)를 추출하여 그래프로 그리는데 라벨을 'Training acc'로 지정하여 그래프에 범례를 추가
     plt.plot(hist['val_acc'], label='Validation acc') # hist 딕셔너리에서 검증 정확도(val_acc)를 추출하여 그래프로 그리는데 라벨을 'Validation acc'로 지정
-    plt.plot(hist['test_acc'], label='Test acc')
+    plt.plot(hist['test_acc'], label='Test acc') # 그래프에 test데이터의 정확도도 표시되게끔 추가하였습니다. 
     plt.legend() # 그래프에 범례를 추가하는데 각 데이터 세트를 구분하기 위해 사용
     plt.subplot(122) # 두 번째 위치(1행 2열의 두 번째)에 또 다른 서브플롯을 생성
     plt.plot(hist['train_loss'], label='Training loss') # hist 딕셔너리에서 학습 손실(train_loss)을 추출하여 그래프로 그린는데 라벨을 'Training loss'로 지정
     plt.plot(hist['val_loss'], label='Validation loss') # hist 딕셔너리에서 검증 손실(val_loss)을 추출하여 그래프로 그리는데 라벨을 'Validation loss'로 지정
-    plt.plot(hist['test_loss'], label='Test loss')
+    plt.plot(hist['test_loss'], label='Test loss') # 그래프에 test데이터의 손실도 표시되게끔 추가하였습니다. 
     plt.legend() # 그래프에 범례를 추가
     
 
